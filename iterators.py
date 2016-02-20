ef30826bfd80ca425edcb1bf283c8574dbeeade4
codenrun <aman.coe@gmail.com>
2016-02-20 23:52:30 +0530

    Added Iterator base class

--- iterators.py
+++ iterators.py
@@ -1,1 +1,21 @@
 
+class Iterator():
+	def __init__(self):
+		pass;
+		
+	def First(self):
+		return None;
+			
+	def Next(self):
+		return None;
+	
+	def Prev(self):
+		return None;
+		
+	def IsDone(self):
+		return true;
+		
+	def CurrentItem(self):
+		return None;
+		
+	
